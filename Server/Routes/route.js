ac407eb1d537d2cfd6da08b6b75bde2cc2ac9892
Akkindian <32745470+Akkindian@users.noreply.github.com>
2017-10-29 09:31:30 -0700

    Update route.js

--- Server/Routes/route.js
+++ Server/Routes/route.js
@@ -77,7 +77,26 @@ router.use(function(req, res, next) {
 router.post('/me', function(req, res) {
     res.json({success: true, message: req.decoded});
 });
-    
+    router.post('/authenticate', function(req, res){
+        User.findOne({username: req.body.username}).select('email username password').exec(function(err, user){
+            if (err) throw err;
+            if(!user) {
+                res.json({success: false,mesage: 'Could not authenticate user'});
+            } else if(user) {
+                if(req.body.password) {
+              var validPassword =  user.comparePassword(req.body.password); }
+               else {
+                res.json({success: false,mesage: 'No password provided'});
+               }
+              if(!validPassword) {
+                  res.json({success: false, message: 'Could not authenticate password'});
+              } else {
+                res.json({success: true, message: 'user authenticated'});
+              }
+            
+            }
+        });
+    });
 
  return router;
  }
