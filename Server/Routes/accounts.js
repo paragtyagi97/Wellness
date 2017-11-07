8c41a86504195ca7968d40e41414b45215781575
Gobind03 <Gobind03@users.noreply.github.com>
2017-11-07 22:03:41 +0530

    Fixed Accounts File

--- Server/Routes/accounts.js
+++ Server/Routes/accounts.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 
 module.exports = function(router){
 
-    router.post('/users', function(req, res){
+    router.post('/registerDoctor', function(req, res){
     
         if(req.body.Username == null || req.body.Username == '' || req.body.Password == null || req.body.Password == '' || req.body.Email == null || req.body.Email == '') {
             res.json({success: true, message: 'Ensure username, email, and password were provided'});
@@ -50,7 +50,7 @@ module.exports = function(router){
 
 
 
-    router.post('/authenticate', function(req, res){
+    router.post('/authenticateDoctor', function(req, res){
         User.findOne({username: req.body.username}).select('email username password').exec(function(err, user){
             if (err) throw err;
             if(!user) {
@@ -69,6 +69,50 @@ module.exports = function(router){
             
             }
         });
+    });
+    
+    //patient basic registeration http://localhost:port/patients/register
+    router.post('/registerPatient', function(req, res){
+        var patient = new Patient(req.body);
+
+
+        if (req.body.username == null || req.body.username == '' || req.body.dob == null || req.body.dob == '' || req.body.email == null || req.body.email == ''
+        || req.body.email == null || req.body.email == '' || req.body.contactNumber == null || req.body.contactNumber == ''){
+            res.json({success: 'false',message: 'Ensure username, email, contact number and date of bierth were provided'});
+        } else {
+        patient.save(function(err){
+            if (err) {
+                res.json({success: 'false',message: 'email already exists!'});//because email attribute set to unique
+            } else {
+                res.json({success: 'true',message: 'Patient registered'});
+            }
+        });
+      
+       }
+
+    }); 
+  
+    //patient basic registeration http://localhost:port/patients/authenticate
+   router.post('/authenticatePatient', function(req, res){
+        Patient.findOne({username: req.body.username}).select('email username password').exec(function(err, user){
+            if (err) throw err;
+            if(!user) {
+                res.json({success: false,mesage: 'Could not authenticate user'});
+            } else if(user) {
+                if(req.body.password) {
+              var validPassword =  user.comparePassword(req.body.password); }
+               else {
+                res.json({success: false,mesage: 'No password provided'});
+               }
+              if(!validPassword) {
+                  res.json({success: false, message: 'Could not authenticate password'});
+              } else {
+                res.json({success: true, message: 'user authenticated'  });
+              }
+            
+            }
+        });
+        
     });
 
  return router;
