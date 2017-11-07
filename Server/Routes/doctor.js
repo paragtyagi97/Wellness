a118701e33cc1fab37d595994583a66e596c97fa
Gobind03 <Gobind03@users.noreply.github.com>
2017-11-07 21:59:53 +0530

    Fixed DoctorJS

--- Server/Routes/doctor.js
+++ Server/Routes/doctor.js
@@ -1,9 +1,4 @@
 var User     = require('../models/doctor');
-var jwt = require('jsonwebtoken');
-var secret = 'meanstack';
-var base64Data;
-var id = require('uniqueid');
-var fs = require("fs");
 
 module.exports = function(doctorRouter){
 
