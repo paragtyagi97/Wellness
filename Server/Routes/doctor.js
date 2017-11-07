80f48622d9a2126e653677334dffd037f5f3bfad
Akkindian <32745470+Akkindian@users.noreply.github.com>
2017-11-07 20:32:36 +0530

    Update doctor.js

--- Server/Routes/doctor.js
+++ Server/Routes/doctor.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 
 module.exports = function(router){
 
-router.post('/users', function(req, res){
+doctorRouter.post('/users', function(req, res){
     
     if(req.body.Username == null || req.body.Username == '' || req.body.Password == null || req.body.Password == '' || req.body.Email == null || req.body.Email == '') {
         res.json({success: true, message: 'Ensure username, email, and password were provided'});
@@ -49,7 +49,7 @@ router.post('/users', function(req, res){
 });
 
 
-router.use(function(req, res, next) {
+doctorRouter.use(function(req, res, next) {
     var token = req.body.token || req.body.query || req.headers['x-access-token'];
     if (token) {
 
@@ -74,7 +74,7 @@ router.use(function(req, res, next) {
 
 
 
-router.post('/me', function(req, res) {
+doctorRouter.post('/me', function(req, res) {
     res.json({success: true, message: req.decoded});
 });
     router.post('/authenticate', function(req, res){
@@ -99,4 +99,4 @@ router.post('/me', function(req, res) {
     });
 
  return router;
- }
+ };
