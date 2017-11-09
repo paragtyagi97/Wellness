f452dc0fc5bd49e2d0ff58babf3fec6119ce2ca5
Akkindian <32745470+Akkindian@users.noreply.github.com>
2017-11-10 00:20:41 +0530

    Update doctor.js

--- Server/Routes/doctor.js
+++ Server/Routes/doctor.js
@@ -1,54 +1,56 @@
-var Doctor    = require('../models/doctor');
+var Doctor     = require('../models/doctor');
+var mongojs = require('mongojs');
 
 module.exports = function(doctorRouter){
 
- //delete doctor login credentials  http://localhost:port/api/doctor/del/:id
-    doctorRouter.get('/del/:id', function(req,res, next){
-		Doctor.findOne({_id: mongojs.ObjectId(req.params.id)}).remove(function(err){
-        if(err)	res.json(err);
-        else { res.json({success: 'true'}); }
-        });
-       
-     
-    }); 
-    //add
-    doctorRouter.put('/add/:id', function(req, res){
-        var doctor = {
-           
-            experience: [{
-                from: req.body.experience.from,
-                to: req.body.experience.to,
-                hospital: req.body.experience.hospital,
-                post: req.body.experience.post,
-                description: req.body.experience.description
-               } ]               
-        };
-       var opts = { strict: false };
-       Doctor.update({_id: mongojs.ObjectId(req.params.id)}, doctor, opts, function(error) {
-         
-         res.json({error: error});
-       });
+//delete doctor login credentials  http://localhost:port/api/doctor/del/:id
+doctorRouter.get('/deleteDoctor/:id', function(req,res, next){
+    Doctor.findOne({_id: mongojs.ObjectId(req.params.id)}).remove(function(err){
+    if(err)	res.json(err);
+    else { res.json({success: 'true'}); }
+    });
    
+ 
+}); 
+//add
+doctorRouter.put('/addExperience/:id', function(req, res){
+    var doctor = {
+       
+        experience: [
+            {from: req.body.experience.from},
+            {to: req.body.experience.to},
+            {hospital: req.body.experience.hospital},
+            {post: req.body.experience.post},
+            {description: req.body.experience.description}
+            ]               
+    };
+   var opts = { strict: false };
+   Doctor.update({_id: mongojs.ObjectId(req.params.id)}, experience, opts, function(error) {
+     if(error){res.json({error: error});}
+    else res.json({sucess:true});
    });
+
+});
 
 //delete data in array
 
-   doctorRouter.put('/remove/:id/:experienceid', function(req,res){
+doctorRouter.put('/removeExperience/:id/:experienceid', function(req,res){
 
-    var my_id = req.params.id,//assume get 54fcb3890cba9c4234f5c925
-    experience_id = req.params.experienceid;// assume get 54fcb3890cba9c4234f5c925
-  
-  Doctor.findByIdAndUpdate(
-    my_id,
-   { $pull: { 'experience': {  _id: experience_id } } },function(err,model){
-      if(err){
-       	console.log(err);
-       	return res.send(err);
-        }
-        return res.json(model);
-    });
+var my_id = req.params.id,//assume get 54fcb3890cba9c4234f5c925
+experience_id = req.params.experienceid;// assume get 54fcb3890cba9c4234f5c925
 
-   });
+Doctor.findByIdAndUpdate(
+my_id,
+{ $pull: { 'experience': {  _id: experience_id } } },function(err,model){
+  if(err){
+       console.log(err);
+       return res.json({error:err});
+    }
+    return res.json({message:"field is deleted"});
+    
+});
+
+});
 
  return doctorRouter;
  };
