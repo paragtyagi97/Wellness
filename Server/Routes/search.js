284ed79b335751548ee7b35b9618076c83fb3c42
Akkindian <32745470+Akkindian@users.noreply.github.com>
2017-11-10 22:09:41 +0530

    Update search.js

--- Server/Routes/search.js
+++ Server/Routes/search.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express');
 var mongojs = require('mongojs');
 var Doctor = require('../models/doctor');
@@ -9,37 +7,11 @@ var mongoose = require('mongoose');
 
 module.exports = function(router){
 
+  //  http://localhost:8080/api/search/doctordata
 router.post('/doctordata',function(req,res){
 
-    if (req.body.City&&req.body.Gender&&req.body.Speciality)
-     {  
-        var query = {
-                   City: req.body.City,
-                   Gender: req.body.Gender,
-                   Speciality: req.body.Speciality
-                     };
-        
-        }
-    
-     if(req.body.City){
-         var query ={ City: req.body.City};
-     }
-     if(req.body.Speciality) {
-      var query = {
-          Speciality: req.body.Speciality
-          
-       };
 
-      }
-       if(req.body.Gender) {
-          var query = {
-              Gender: req.body.Gender
-             
-           };
-        }
-
-
-    Doctor.findOne(query,function(err, result) {
+    Doctor.findOne(req.body,function(err, result) {
       if (err) throw err;
       res.json({sucess:true, result: result});
       
