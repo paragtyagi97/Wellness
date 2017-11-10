6751a16d36e65ea3c5efe1db6bb7084c7b7d1dc0
Akkindian <32745470+Akkindian@users.noreply.github.com>
2017-11-10 19:53:16 +0530

    Update search.js

--- Server/Routes/search.js
+++ Server/Routes/search.js
@@ -5,23 +5,40 @@ var mongojs = require('mongojs');
 var collection = require('../models/doctor');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-var Router = express.Router();
+
 
 module.exports = function(router){
 
 router.post('/doctordata',function(req,res){
-       var search = {
+      if (req.body.City&&req.body.Gender&&req.body.Speciality) {var search = {
           City: req.body.City,
           Gender: req.body.Gender,
           Speciality: req.body.Speciality, 
           _id: req.body._id
-       };
+       };}
+       if(req.body.City){
+           var search ={ City: req.body.City, _id: req.body._id};
+       }
+       if(req.body.Speciality) {
+        var search = {
+            Speciality: req.body.Speciality, 
+            _id: req.body._id
+         };
+
+        }
+         if(req.body.Gender) {
+            var search = {
+                Gender: req.body.Gender, 
+                _id: req.body._id
+             };
+
+       }
      // console.log(req.params.id);
       collection.findOne({_id: req.body._id},search,function(err, doc)
       {
          if (doc){
              console.log(doc);
-             res.json({sucess: true, model: doc});
+            // res.json({sucess: true});
          } else {
              console.log('not found');
              res.json({sucess: false, message: "not found"});
