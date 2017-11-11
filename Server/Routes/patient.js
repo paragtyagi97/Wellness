4283a3405ae1a7682506dd495f2e591651f92103
Akkindian <32745470+Akkindian@users.noreply.github.com>
2017-11-11 12:25:24 +0530

    Update patient.js

--- Server/Routes/patient.js
+++ Server/Routes/patient.js
@@ -23,6 +23,42 @@ patientRouter.put('/updatePatient/:id', function(req, res){
   }
 
 });
+     patientRouter.post('/updatePatient/password', function(req, res){
+  
+   //  var username = req.body.username;
+     var password = req.body.password;
+  
+     Patient.findOne({username: req.body.username}).select('email username password').exec(function(err, user){
+      if (err) throw err;
+      if(!user) {
+          res.json({success: false,mesage: 'Could not find user'});
+      } else if(user) {
+          if(req.body.password) {
+        var validPassword =  user.comparePassword(req.body.password); }
+         else {
+          res.json({success: false,mesage: 'No password provided '});
+         }
+        if(!validPassword) {
+            res.json({success: false, message: 'Could not match password'});
+        } else {
+
+          var opts = { strict: false };
+
+          Patient.update({username: req.body.username}, {password: req.body.newPassword} , opts, function(error) {
+            if(error) {res.json({error:error});}
+            else {
+            res.json({success:true,message: "Sucessfully updated"});
+            console.log("updated"); }
+            
+          }); 
+
+
+
+        
+        }
+      
+      }
+  });
 
 
 return patientRouter; 
