bb095ef480071a9004e2c975d09e9d51b859d993
Akkindian <32745470+Akkindian@users.noreply.github.com>
2017-11-11 14:12:52 +0530

    Update doctor.js

--- Server/Routes/doctor.js
+++ Server/Routes/doctor.js
@@ -22,8 +22,7 @@ doctorRouter.put('/addExperience/:id', function(req, res){
             hospital: req.body.experience.hospital,
             post: req.body.experience.post,
             description: req.body.experience.description
-        }]
-                           
+        }]               
     };
    var opts = { strict: false };
    Doctor.update({_id: mongojs.ObjectId(req.params.id)}, experience, opts, function(error) {
@@ -45,13 +44,59 @@ my_id,
 { $pull: { 'experience': {  _id: experience_id } } },function(err,model){
   if(err){
        console.log(err);
-       return res.json({error:err});
+       return res.json({err:err});
     }
     return res.json({message:"field is deleted"});
     
 });
 
 });
+
+
+doctorRouter.post('/updatePassword', function(req, res){
+    
+     //  var username = req.body.username;
+       var Password = req.body.Password;
+    
+       Doctor.findOne({username: req.body.UserName}).select('email username password').exec(function(err, user){
+        if (err) throw err;
+        if(!user) {
+            res.json({success: false,mesage: 'Could not find user'});
+        } else if(user) {
+            if(req.body.Password) {
+          var validPassword =  user.comparePassword(req.body.Password); 
+         // var NewPassword =  user.hashPassword(req.body.newPassword);
+        }
+           else {
+            res.json({success: false,mesage: 'No password provided '});
+           }
+          if(!validPassword) {
+              res.json({success: false, message: 'Could not match password'});
+          } else {
+  
+            var opts = { strict: false };
+            
+            var Newpassword = req.body.newPassword;
+            
+            // update it with hash
+            req.body.newPassword = bcrypt.hashSync(Newpassword); 
+  
+            Patient.update({UserName: req.body.UserName}, {Password: req.body.newPassword} , opts, function(error) {
+              if(error) {res.json({error:error});}
+              else {
+              res.json({success:true,message: "Sucessfully updated"});
+              console.log("updated"); }
+              
+            }); 
+  
+          }
+        
+        }
+    });
+  
+  
+   
+   });
 
  return doctorRouter;
  };
